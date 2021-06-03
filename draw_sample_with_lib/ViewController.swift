efd8ffa57f776e168d7a0f2128d5069ce988a170
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2021-06-04 00:47:19 +0900

    Initial Commit

---
+++ draw_sample_with_lib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  draw_sample_with_lib
+//
+//  Created by 平塚俊輔 on 2021/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ draw_sample_with_libTests/draw_sample_with_libTests.swift
@@ -0,0 +1,33 @@
+//
+//  draw_sample_with_libTests.swift
+//  draw_sample_with_libTests
+//
+//  Created by 平塚俊輔 on 2021/06/04.
+//
+
+import XCTest
+@testable import draw_sample_with_lib
+
+class draw_sample_with_libTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
