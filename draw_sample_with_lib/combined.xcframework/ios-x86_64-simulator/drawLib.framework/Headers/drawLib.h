b85730617edc99d68a19170b8d5f1b878b8b6663
shiratsu <shun_hiratsuka2002@yahoo.co.jp>
2021-06-05 00:23:34 +0900

    first commit

---
+++ draw_sample_with_lib/combined.xcframework/ios-x86_64-simulator/drawLib.framework/Headers/drawLib.h
@@ -0,0 +1,18 @@
+//
+//  drawLib.h
+//  drawLib
+//
+//  Created by 平塚俊輔 on 2021/06/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for drawLib.
+FOUNDATION_EXPORT double drawLibVersionNumber;
+
+//! Project version string for drawLib.
+FOUNDATION_EXPORT const unsigned char drawLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <drawLib/PublicHeader.h>
+
+
